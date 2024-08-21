f3fb82a3c85d37d9f45a552befee71a1d2e099d6
shinaaaa <shin029abc@gmail.com>
2024-08-21 23:00:04 +0900

    feat: 게시글, 회원 업데이트

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -51,4 +51,36 @@ public class MemberController {
         model.addAttribute("memberList", memberEntityList);
         return "members/index";
     }
+
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+
+        // 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+
+        // 뷰페이지 설정하기
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form) {
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환하기
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            memberRepository.save(memberEntity); // 엔티티를 DB에 저장(갱신)
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/members/" + memberEntity.getId();
+    }
 }
